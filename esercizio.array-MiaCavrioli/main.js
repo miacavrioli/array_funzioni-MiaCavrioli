1fe58b6373924a3cdd30cff4c028efdae17b11f2
miacavrioli <miacavrioli@gmail.com>
2024-12-12 21:13:43 +0100

    Es. Array e Funzioni

---
+++ esercizio.array-MiaCavrioli/main.js
@@ -0,0 +1,29 @@
+//Esercizi su array e funzioni
+
+// 1. Funzione
+
+/* function FizzBuzzN(N) {
+    for (let i = 1; i <= N; i++) {
+        if ( i %3 == 0 && i %5 == 0) {
+            console.log("fizzBuzz");}
+        else if ( i %5 == 0) {
+            console.log("Buzz");}
+    
+        else if ( i %3 == 0) {
+            console.log("Fizz");}
+        else {
+            console.log(i);}
+    }}
+
+FizzBuzzN (20); */
+
+// 2. Sconto
+
+/* function discount (price, discount){
+    result = price*discount/100;
+    console.log(`Il prezzo dopo lo sconto è di ${result}`);
+}
+
+discount (10, 50) */
+
+
